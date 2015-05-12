8e1ad4dc0fc78f27f19a090860a5fc92a42debac
numminorihsf <numminorihsf@gmail.com>
2015-05-12 14:22:20 +0700

    now it works.  can use mono or multi thread

--- index.js
+++ index.js
@@ -26,5 +26,77 @@ if (module.parent) {
     module.exports = require(__dirname+'/lib/index.js');
 }
 else {
+    var servers = [];
+    var needMultitread = false;
 
+    var readline = require('readline');
+
+    var rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    function addServer(callback){
+        rl.question("AMI host [127.0.0.1]: ", function(host) {
+            host = host || '127.0.0.1';
+            rl.question("AMI port [5038]: ", function(port) {
+                port = port || '5038';
+                rl.question("AMI login [admin]: ", function(login) {
+                    login = login || 'admin';
+                    rl.question("AMI password [admin]: ", function(password) {
+                        password = password || 'admin';
+                        servers.push({host:host, port:port, login:login, password:password});
+                        rl.question("Do you want another AMI?\n[y(Yes)/N(No)]: ", function(yesNo) {
+                            yesNo = yesNo.toLowerCase();
+                            if (yesNo !== 'y' && yesNo !== 'yes') return callback();
+                            else return addServer(callback);
+                        });
+                    });
+                });
+            });
+        });
+
+    }
+    function setMultitread(callback){
+        rl.question("Do you want start workers in multi threads?\n[y(Yes)/N(No)]: ", function(yesNo) {
+            yesNo = yesNo.toLowerCase();
+            rl.close();
+            if (yesNo == 'y' || yesNo == 'yes') return callback(true);
+            else return callback(false);
+        });
+    }
+    function startAmis(multi){
+        if (multi) master = new (require(__dirname+'/lib/index.js').multi)();
+        else master = new (require(__dirname+'/lib/index.js').mono)();
+        var enabledEvents = ['connected', 'connectionRefused', 'incorrectServer', 'incorrectLogin', 'disconnect', 'ais.CallStart'];
+        var silence = function(){};
+        for(var i in servers){
+            servers[i].enabledEvents = enabledEvents;
+            servers[i].logger = {trace: silence, debug: silence, info:silence, warn:silence, error: silence, fatal: silence};
+            master.openAmi(i, servers[i]);
+            master.on('ami.connected', function(e){
+                console.log(i, 'connected');
+            });
+            master.on('ami.ais.CallStart', function(e){
+                console.log(e);
+            });
+            master.on('error', function(err){
+                console.error('E:', err);
+            });
+        }
+    }
+
+    addServer(function(){
+        setMultitread(function(multi){
+            startAmis(multi);
+        })
+    });
+
+    process.on('SIGTERM', function(){
+        process.exit();
+    });
+
+    process.on('SIGINT', function(){
+        process.exit();
+    });
 }
